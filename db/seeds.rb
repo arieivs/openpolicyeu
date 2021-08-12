207eae077475c346b5af5f2a02782f187cafa2ba
Sara Vieira <s.cvieira@sapo.pt>
2021-08-12 15:58:00 +0100

    added volunteers profile pic with cloudinary, added faker gem and did volunteer seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'open-uri'
+
+puts 'Creating volunteers'
+5.times do
+  new_volunteer = Volunteer.new(name: Faker::Superhero.name, role: Faker::Superhero.power, linked_link:"")
+  file = URI.open("https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80")
+  new_volunteer.photo.attach(io: file, filename: "profilepic.png", content_type: 'image/png')
+  new_volunteer.save
+end
+
+puts 'Done'
