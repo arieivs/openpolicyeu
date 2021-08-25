61dad32e821c3faf6817ec5cc03c84092f18edad
baptjs <baptiste.josse@gmail.com>
2021-08-25 17:33:06 +0200

    institutions routes & controller & new/edit forms

--- app/models/institution.rb
+++ app/models/institution.rb
@@ -4,6 +4,8 @@ class Institution < ApplicationRecord
   has_many :policy_making_institutions
   has_many :policy_makings, through: :policy_making_institutions
 
+  has_one_attached :logo
+
   validates :name, presence: true
   validates :description, presence: true
   validates :website_url, presence: true
