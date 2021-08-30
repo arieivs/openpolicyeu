93333b8d0dd6badefac7eefdc56d842b8da2e2fb
Sara Vieira <s.cvieira@sapo.pt>
2021-08-30 16:40:52 +0100

    changed description from institutions to policymakinginstitutions

--- app/models/institution.rb
+++ app/models/institution.rb
@@ -7,6 +7,5 @@ class Institution < ApplicationRecord
   has_one_attached :logo
 
   validates :name, presence: true
-  validates :description, presence: true
   validates :website_url, presence: true
 end
--- app/models/policy_making_institution.rb
+++ app/models/policy_making_institution.rb
@@ -1,4 +1,6 @@
 class PolicyMakingInstitution < ApplicationRecord
   belongs_to :policy_making
   belongs_to :institution
+
+  validates :description, presence: true
 end
