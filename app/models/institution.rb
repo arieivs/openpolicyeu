1b3a63f4303843cc288b3a3edd2d4698497c291a
Sara Vieira <s.cvieira@sapo.pt>
2021-08-30 17:41:57 +0100

    seeds for institutions, questions and answers

--- app/models/institution.rb
+++ app/models/institution.rb
@@ -1,6 +1,5 @@
 class Institution < ApplicationRecord
   belongs_to :country
-  belongs_to :ambassador
   has_many :policy_making_institutions
   has_many :policy_makings, through: :policy_making_institutions
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,35 +8,55 @@
 
 require 'open-uri'
 
+# --------------- CLEANING ---------------
 
-# --------------- COUNTRIES ---------------
-
+puts "Cleaning ANSWERS database..."
+Answer.destroy_all
+puts "Cleaning QUESTIONS database..."
+Question.destroy_all
+puts "Cleaning POLICY_MAKINGS database..."
+PolicyMakingInstitution.destroy_all
+PolicyMaking.destroy_all
+# puts "Cleaning AMBASSADORS database..."
+# Ambassador.destroy_all
+puts "Cleaning INSTITUIONS database..."
+Institution.destroy_all
 puts "Cleaning COUNTRIES database..."
 Country.destroy_all
-
-puts "Creating countries..."
-COUNTRIES = ['Europe', 'Austria', 'Belgium', 'Bulgaria', 'Croatia', 'Republic of Cyprus', 'Czech Republic', 'Denmark', 'Estonia', 'Finland', 'France', 'Germany', 'Greece', 'Hungary', 'Ireland', 'Italy', 'Latvia', 'Lithuania', 'Luxembourg', 'Malta', 'Netherlands', 'Poland', 'Portugal', 'Romania', 'Slovakia', 'Slovenia', 'Spain', 'Sweden']
-COUNTRIES.each do |country|
-  Country.new(name: country).save # Add flags ?
-end
-puts "#{Country.count} countries created! \n\n"
+puts "Cleaning TOPICS database..."
+Topic.destroy_all
+puts "Cleaning VOLUNTEERS database..."
+Volunteer.destroy_all
+puts "Empty database \n\n"
 
 # --------------- TOPICS ---------------
 
-puts "Cleaning TOPICS database..."
-Topic.destroy_all
-
 puts "Creating topics..."
-TOPICS = ['Energy', 'Climate', 'Education', 'Health & food safety', 'Mobility', 'Interregional cooperation', 'Defense, Industry, Space']
+# TOPICS = ['Energy', 'Climate', 'Education', 'Health & food safety', 'Mobility', 'Interregional cooperation', 'Defense, Industry, Space']
+TOPICS = ['Energy', 'Climate', 'Youth']
 TOPICS.each do |topic|
   Topic.new(name: topic).save
 end
 puts "#{Topic.count} topics created! \n\n"
 
-# --------------- AMBASSADORS ---------------
+# ------- COUNTRIES & INSTITUTIONS -------
 
-# puts "Cleaning AMBASSADORS database..."
-# Ambassador.destroy_all
+puts "Creating countries and institutions..."
+# COUNTRIES = ['Europe', 'Austria', 'Belgium', 'Bulgaria', 'Croatia', 'Republic of Cyprus', 'Czech Republic', 'Denmark', 'Estonia', 'Finland', 'France', 'Germany', 'Greece', 'Hungary', 'Ireland', 'Italy', 'Latvia', 'Lithuania', 'Luxembourg', 'Malta', 'Netherlands', 'Poland', 'Portugal', 'Romania', 'Slovakia', 'Slovenia', 'Spain', 'Sweden']
+COUNTRIES = ['Europe', 'Germany', 'Portugal', 'Italy', 'Slovenia']
+COUNTRIES.each do |country|
+  new_country = Country.new(name: country) # Add flags ?
+  new_country.save
+  3.times do
+    new_institution = Institution.new(country: new_country, name: Faker::Company.name, website_url: Faker::Internet.url, video_url: "https://www.youtube.com/watch?v=BUMyjwCMzSI", video_source: "EU Council", video_alt_text: "bla")
+    file = URI.open("https://globalaccessibilitynews.com/files/2013/03/European-Commission-logo.png")
+    new_institution.logo.attach(io: file, filename: "profilepic.png", content_type: 'image/png')
+    new_institution.save
+  end
+end
+puts "#{Country.count} countries and #{Institution.count} institutions created! \n\n"
+
+# --------------- AMBASSADORS ---------------
 
 # puts "Creating ambassadors..."
 # 5.times do
@@ -45,48 +65,60 @@ puts "#{Topic.count} topics created! \n\n"
 #   new_ambassador.photo.attach(io: file, filename: "profilepic.png", content_type: 'image/png')
 #   new_ambassador.save
 # end
-# puts "#{Ambassador.count} ambassadors created! \n\n"
-
-# --------------- INSTITUIONS ---------------
 
 # --------------- POLICY MAKINGS ---------------
 
-puts "Cleaning POLICY_MAKINGS database..."
-PolicyMaking.destroy_all
+puts "Creating policymakings..."
+pms = []
+pm = PolicyMaking.new(country: Country.first, topic: Topic.first, video_url: "https://www.youtube.com/watch?v=BUMyjwCMzSI", video_source: "EU Council", content: Faker::Lorem.sentence(word_count: 50, supplemental: false, random_words_to_add: 10))
+pm.save
+pms.push(pm)
+pm = PolicyMaking.new(country: Country.first, topic: Topic.second, video_url: "https://www.youtube.com/watch?v=BUMyjwCMzSI", video_source: "EU Council", content: Faker::Lorem.sentence(word_count: 50, supplemental: false, random_words_to_add: 10))
+pm.save
+pms.push(pm)
+pm = PolicyMaking.new(country: Country.first, topic: Topic.third, video_url: "https://www.youtube.com/watch?v=BUMyjwCMzSI", video_source: "EU Council", content: Faker::Lorem.sentence(word_count: 50, supplemental: false, random_words_to_add: 10))
+pm.save
+pms.push(pm)
+pm = PolicyMaking.new(country: Country.second, topic: Topic.second, video_url: "https://www.youtube.com/watch?v=BUMyjwCMzSI", video_source: "EU Council", content: Faker::Lorem.sentence(word_count: 50, supplemental: false, random_words_to_add: 10))
+pm.save
+pms.push(pm)
+pm = PolicyMaking.new(country: Country.second, topic: Topic.third, video_url: "https://www.youtube.com/watch?v=BUMyjwCMzSI", video_source: "EU Council", content: Faker::Lorem.sentence(word_count: 50, supplemental: false, random_words_to_add: 10))
+pm.save
+pms.push(pm)
+pm = PolicyMaking.new(country: Country.third, topic: Topic.third, video_url: "https://www.youtube.com/watch?v=BUMyjwCMzSI", video_source: "EU Council", content: Faker::Lorem.sentence(word_count: 50, supplemental: false, random_words_to_add: 10))
+pm.save
+pms.push(pm)
+puts "#{PolicyMaking.count} policymakings created! \n\n"
 
-puts "Creating policy makings..."
-PolicyMaking.new(country_id: 1, topic_id: 1, video_url: "https://www.youtube.com/watch?v=BUMyjwCMzSI", video_source: "EU Council", content: Faker::Lorem.sentence(word_count: 50, supplemental: false, random_words_to_add: 10)).save
-PolicyMaking.new(country_id: 1, topic_id: 2, video_url: "https://www.youtube.com/watch?v=BUMyjwCMzSI", video_source: "EU Council", content: Faker::Lorem.sentence(word_count: 50, supplemental: false, random_words_to_add: 10)).save
-PolicyMaking.new(country_id: 1, topic_id: 3, video_url: "https://www.youtube.com/watch?v=BUMyjwCMzSI", video_source: "EU Council", content: Faker::Lorem.sentence(word_count: 50, supplemental: false, random_words_to_add: 10)).save
-PolicyMaking.new(country_id: 2, topic_id: 1, video_url: "https://www.youtube.com/watch?v=BUMyjwCMzSI", video_source: "EU Council", content: Faker::Lorem.sentence(word_count: 50, supplemental: false, random_words_to_add: 10)).save
-PolicyMaking.new(country_id: 3, topic_id: 1, video_url: "https://www.youtube.com/watch?v=BUMyjwCMzSI", video_source: "EU Council", content: Faker::Lorem.sentence(word_count: 50, supplemental: false, random_words_to_add: 10)).save
-PolicyMaking.new(country_id: 4, topic_id: 1, video_url: "https://www.youtube.com/watch?v=BUMyjwCMzSI", video_source: "EU Council", content: Faker::Lorem.sentence(word_count: 50, supplemental: false, random_words_to_add: 10)).save
-puts "#{PolicyMaking.count} policy makings created! \n\n"
+puts "Associating institutions to policymakings..."
+pms.each do |pm|
+  institutions = Institution.where(country: pm.country)
+  institutions.each do |i|
+    PolicyMakingInstitution.new(policy_making: pm, institution: i, description: Faker::Lorem.sentence(word_count: 50, supplemental: false, random_words_to_add: 10)).save
+  end
+end
+puts "#{PolicyMakingInstitution.count} policymaking_institutions created! \n\n"
 
 # ----------- QUESTIONS & ANSWERS ----------
 
-puts "Cleaning QUESTIONS & ANSWERS database..."
-Answer.destroy_all
-Question.destroy_all
-
-puts "Creating questions and answers for policymaking..."
-(1..PolicyMaking.count).to_a.each do |pm_id|
-  question_content = Question.new(policy_making_id: pm_id, scope: 'content', question: "#{Faker::Quotes::Shakespeare.hamlet}?").save
-  Answer.new(question: question_content, answer: Faker::TvShows::BojackHorseman.quote, explanation: Faker::Movies::HitchhikersGuideToTheGalaxy.quote, right: true).save
+puts "Creating questions and answers for policymakings..."
+pms.each do |pm|
+  content_question = Question.new(policy_making: pm, scope: 'content', question: "#{Faker::Quotes::Shakespeare.hamlet}?")
+  content_question.save
+  Answer.new(question: content_question, answer: Faker::TvShows::BojackHorseman.quote, explanation: Faker::Movies::HitchhikersGuideToTheGalaxy.quote, right: true).save
   rand(2..4).times do
-    Answer.new(question: question_content, answer: Faker::TvShows::BojackHorseman.quote, explanation: Faker::Movies::HitchhikersGuideToTheGalaxy.quote, right: false).save
+    Answer.new(question: content_question, answer: Faker::TvShows::BojackHorseman.quote, explanation: Faker::Movies::HitchhikersGuideToTheGalaxy.quote, right: false).save
   end
-  question_institution = Question.new(policy_making_id: pm_id, scope: 'institution', question: "#{Faker::Quotes::Shakespeare.hamlet}?").save
+  question_institution = Question.new(policy_making: pm, scope: 'institutions', question: "#{Faker::Quotes::Shakespeare.hamlet}?")
+  question_institution.save
   Answer.new(question: question_institution, answer: Faker::TvShows::BojackHorseman.quote, explanation: Faker::Movies::HitchhikersGuideToTheGalaxy.quote, right: true).save
   rand(2..4).times do
     Answer.new(question: question_institution, answer: Faker::TvShows::BojackHorseman.quote, explanation: Faker::Movies::HitchhikersGuideToTheGalaxy.quote, right: false).save
   end
 end
+puts "#{Question.count} questions and #{Answer.count} answers created! \n\n"
 
 # --------------- VOLUNTEERS ---------------
-
-puts "Cleaning VOLUNTEERS database..."
-Volunteer.destroy_all
 
 puts "Creating volunteers..."
 5.times do
