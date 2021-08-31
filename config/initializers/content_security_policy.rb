18ba33a9949ddd47651b21475b4d6139c46c405d
Sara Vieira <s.cvieira@sapo.pt>
2021-09-01 00:18:37 +0100

    fixed, the security policies were blocking it. institution descriptions now change on click

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -4,11 +4,17 @@
 # For further information see the following documentation
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
 
+=begin
+
+Copied this here trying to render the iframes
+In the end, I couldn't render images from Cloudinary nor my very own JavaScript
+To be further investigated
+
 Rails.application.config.content_security_policy do |policy|
   policy.default_src :self, :https
   policy.font_src    :self, :https, :data
   policy.img_src     :self, :https, :data
-  policy.object_src  :none
+  policy.object_src  :self
   policy.script_src  :self, :https
   policy.style_src   :self, :https
   policy.frame_src   :self, "https://www.youtube.com"
@@ -19,6 +25,8 @@ Rails.application.config.content_security_policy do |policy|
   # Specify URI for violation reports
   # policy.report_uri "/csp-violation-report-endpoint"
 end
+
+=end
 
 # If you are using UJS then enable automatic nonce generation
 # Rails.application.config.content_security_policy_nonce_generator = -> request { SecureRandom.base64(16) }
