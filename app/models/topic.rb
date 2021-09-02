9f85abebdb60d7f29b6c1196e5cf5528b6f4a225
Sara Vieira <s.cvieira@sapo.pt>
2021-09-03 00:33:53 +0100

    cards on landing page done

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,4 +2,6 @@ class Topic < ApplicationRecord
   has_many :policy_makings
   has_many :categories
   validates :name, presence: true
+  validates :icon, presence: true
+  validates :icon_color, presence: true
 end
