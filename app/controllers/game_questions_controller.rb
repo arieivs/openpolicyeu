5753603d4fc099ac383ff944111a6ef15df406cc
Sara Vieira <s.cvieira@sapo.pt>
2021-09-13 11:40:17 +0100

    adding policy_plan_id to the params sent to select_answer_gamebook. in the gameboook you can only answer the question once

--- app/controllers/game_questions_controller.rb
+++ app/controllers/game_questions_controller.rb
@@ -3,9 +3,14 @@ class GameQuestionsController < ApplicationController
 
   def select_answer_gamebook
     # when the user clicks in one of the answers from the Gamebook
+    # for showing the if the answer is right or wrong + explanation:
     @game_question = GameQuestion.find(params[:game_question_id])
     @game_answers = GameAnswer.where(game_question: @game_question)
     @selected_answer = GameAnswer.find(params[:game_answer_id])
+    # for showing next question:
+    @game_questions = GameQuestion.where(policy_plan_id: params[:policy_plan_id])
+    # @next_question = ?
+    # @next_answers = ?
     # then it renders game_questions > select_answer_gamebook.js.erb automatically
   end
 end
