78d069ee788ea0fab78105014e837f76397b35d6
Sara Vieira <s.cvieira@sapo.pt>
2021-09-14 16:18:59 +0100

    new action and view for policy plans

--- app/controllers/policy_plans_controller.rb
+++ app/controllers/policy_plans_controller.rb
@@ -15,6 +15,19 @@ class PolicyPlansController < ApplicationController
     @young_contributors = YoungContributor.where(policy_plan: @policy_plan)
   end
 
+  def new
+    @policy_plan = PolicyPlan.new
+  end
+
+  def create
+    @policy_plan = PolicyPlan.new(policy_plan_params)
+    if @policy_plan.save
+      redirect_to edit_policy_plan_path(@policy_plan)
+    else
+      render :new
+    end
+  end
+
   def choose_institution
     # when the user clicks in one of the institutions cards
     # we call this method and pass the needed ids in the params
@@ -60,5 +73,9 @@ class PolicyPlansController < ApplicationController
     @game_questions = @policy_plan.strategy ? @game_questions.order(:order) : @game_questions.order(:date)
     @next_question = @game_questions.first
     @next_answers = GameAnswer.where(game_question: @next_question)
+  end
+
+  def policy_plan_params
+    params.require(:policy_plan).permit(:policy_making_id, :name, :short_description, :content, :strategy, :video_url, :video_alt_text, :video_source, :ambassador_id)
   end
 end
