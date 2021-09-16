3840abf0214152f481a2d7b13fc668c2ef053d5a
Sara Vieira <s.cvieira@sapo.pt>
2021-09-16 19:24:27 +0100

    editing game answers

--- app/controllers/game_answers_controller.rb
+++ app/controllers/game_answers_controller.rb
@@ -1,2 +1,18 @@
 class GameAnswersController < ApplicationController
+  def edit
+    @game_answer = GameAnswer.find(params[:id])
+    respond_to { |format| format.js }
+  end
+
+  def update
+    @game_answer = GameAnswer.find(params[:id])
+    @game_answer.update(game_answer_params)
+    respond_to { |format| format.js }
+  end
+
+  private
+
+  def game_answer_params
+    params.require(:game_answer).permit(:context, :answer, :explanation, :right)
+  end
 end
