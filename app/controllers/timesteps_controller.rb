a97d9bd582bb23290a3ab4355c821507fff7add3
Sara Vieira <s.cvieira@sapo.pt>
2021-09-16 12:18:27 +0100

    edit policy plans timesteps done

--- app/controllers/timesteps_controller.rb
+++ app/controllers/timesteps_controller.rb
@@ -3,9 +3,16 @@ class TimestepsController < ApplicationController
   end
 
   def edit
+    @timestep = Timestep.find(params[:id])
+    respond_to { |format| format.js }
   end
 
   def update
+    @timestep = Timestep.find(params[:id])
+    @timestep.update(timestep_params)
+    @policy_plan = @timestep.policy_plan
+    @timesteps = Timestep.where(policy_plan: @policy_plan).order(:date)
+    respond_to { |format| format.js }
   end
 
   def destroy
@@ -14,5 +21,11 @@ class TimestepsController < ApplicationController
     @timestep.destroy
     @timesteps = Timestep.where(policy_plan: @policy_plan).order(:date)
     respond_to { |format| format.js }
+  end
+
+  private
+
+  def timestep_params
+    params.require(:timestep).permit(:date, :name, :description, :policy_plan_id)
   end
 end
