bae7a06bc84dc87c853b734b24622296946c20e8
Sara Vieira <s.cvieira@sapo.pt>
2021-09-16 11:36:32 +0100

    destroying timesteps

--- app/controllers/timesteps_controller.rb
+++ app/controllers/timesteps_controller.rb
@@ -9,5 +9,10 @@ class TimestepsController < ApplicationController
   end
 
   def destroy
+    @timestep = Timestep.find(params[:id])
+    @policy_plan = @timestep.policy_plan
+    @timestep.destroy
+    @timesteps = Timestep.where(policy_plan: @policy_plan).order(:date)
+    respond_to { |format| format.js }
   end
 end
