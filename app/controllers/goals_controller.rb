0490299d6636b19bf46ebcb9d39a93b9f692656c
Sara Vieira <s.cvieira@sapo.pt>
2021-09-17 00:27:51 +0100

    destroy goals done

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -10,6 +10,21 @@ class GoalsController < ApplicationController
     respond_to { |format| format.js }
   end
 
+  def destroy
+    @goal = Goal.find(params[:id])
+    @policy_plan = @goal.policy_plan
+    @goal.destroy
+    @goals = Goal.where(policy_plan: @policy_plan).order(:order)
+    @goals_n_games = []
+    @goals.each do |goal|
+      game_question = GameQuestion.where(policy_plan: @policy_plan).find_by(order: goal.order)
+      game_answers = GameAnswer.where(game_question: game_question)
+      @goals_n_games.push({ goal: goal, game_question: game_question, game_answers: game_answers })
+    end
+    @new_game_answer = GameAnswer.new
+    respond_to { |format| format.js }
+  end
+
   private
 
   def goal_params
