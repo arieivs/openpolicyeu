422609fe00acbfbbfbf3f10f7bb20307c62b96a9
Sara Vieira <s.cvieira@sapo.pt>
2021-09-16 22:51:49 +0100

    displaying goals in policy plans edit

--- app/controllers/policy_plans_controller.rb
+++ app/controllers/policy_plans_controller.rb
@@ -105,5 +105,13 @@ class PolicyPlansController < ApplicationController
       @game_answers[game_question.id] = GameAnswer.where(game_question: game_question)
     end
     @new_game_answer = GameAnswer.new
+    @goals = Goal.where(policy_plan: @policy_plan)
+    @goals_n_games = []
+    if @policy_plan.strategy
+      @goals.each do |goal|
+        game_question = @game_questions.find_by(order: goal.order)
+        @goals_n_games.push({ goal: goal, game_question: game_question, game_answers: @game_answers[game_question.id] })
+      end
+    end
   end
 end
