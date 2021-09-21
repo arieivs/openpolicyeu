653bdee91f8d3aa776d98b4ffe7ecb8e0f19bc09
Sara Vieira <s.cvieira@sapo.pt>
2021-09-21 12:26:37 +0100

    properly validating if answer and gameanswer have right field

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,5 +2,5 @@ class Answer < ApplicationRecord
   belongs_to :question
   validates :answer, presence: true
   validates :explanation, presence: true
-  validates :right, presence: true
+  validates :right, inclusion: { in: [true, false] }
 end
--- app/models/game_answer.rb
+++ app/models/game_answer.rb
@@ -2,4 +2,5 @@ class GameAnswer < ApplicationRecord
   belongs_to :game_question
   validates :answer, presence: true
   validates :explanation, presence: true
+  validates :right, inclusion: { in: [true, false] }
 end
