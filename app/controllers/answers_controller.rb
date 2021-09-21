320612d4944eb974660113bd836a2f37063b4311
Sara Vieira <s.cvieira@sapo.pt>
2021-09-21 11:26:08 +0100

    error handling for creating or editing answers without all required fields done

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -3,9 +3,12 @@ class AnswersController < ApplicationController
     @question = Question.find(params[:question_id])
     @answer = Answer.new(answer_params)
     @answer.question = @question
-    @answer.save
     @new_answer = Answer.new
-    respond_to { |format| format.js }
+    if @answer.save
+      respond_to { |format| format.js }
+    else
+      respond_to { |format| format.js { flash[:alert] = "Please add all the required fields to create an answer." } }
+    end
   end
 
   def edit
@@ -15,8 +18,11 @@ class AnswersController < ApplicationController
 
   def update
     @answer = Answer.find(params[:id])
-    @answer.update(answer_params)
-    respond_to { |format| format.js }
+    if @answer.update(answer_params)
+      respond_to { |format| format.js }
+    else
+      render :edit
+    end
   end
 
   def destroy
