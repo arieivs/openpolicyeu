ef7d5c9b2e11ac8f9df1dd700d56c425e51634d5
David Carmo <dmcarmo@gmail.com>
2021-11-11 16:24:46 +0000

    Fixes error with organisation filters

--- app/controllers/volunteers_controller.rb
+++
@@ -1,7 +0,0 @@
-class VolunteersController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index]
-
-  def index
-    @volunteers = Volunteer.includes(photo_attachment: :blob).order(:id)
-  end
-end
